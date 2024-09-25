f8b2c97ddf7dc760096a0581fb15111878385e94
mariana-moraes <A50560@alunos.isel.pt>
2024-09-25 12:16:05 +0100

    execute commands

--- bach.c
+++ bach.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+<<<<<<< Updated upstream
 #include <sys/wait.h>
 
 #define MAX_LINE 1024
 #define MAX_ARGS 64
+=======
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_LINE 1024
+#define MAX_ARGS 20
+>>>>>>> Stashed changes
 #define TRUE 1
 
 /**
@@ -20,6 +28,7 @@ typedef struct bach_command_t {
     char *redir_out;
 } BachCommand, *PBachCommand;
 
+
 //add args to main
 int main() {
     while (TRUE) {
@@ -31,4 +40,25 @@ int main() {
         // parse the command
         // fun void xpto(BachCommand *cmd) {TODO()}
     }
+}
+
+void executecmd(PBachCommand cmd){	
+    int child_pid;
+
+    if ((child_pid = fork()) == -1) {
+		perror("can't create new process");
+		exit(1);
+	}
+	else if (child_pid == 0) {
+		if (execvp(cmd.name, cmd.args) == -1) {
+			perror2("error on exec %s", cmd.name);
+			exit(1);
+		}
+	
+	}
+	else {
+		waitpid(child_pid, NULL, 0);
+	}
+
+    return;
 }
