7840eea289b3ab26bdefe1a481c6a60fc8b9737e
Arthur Oliveira <arthurcnoliveira@gmail.com>
2024-09-28 21:13:38 +0100

    bach_test correction

--- bach_test.c
+++ bach_test.c
@@ -1,7 +1,9 @@
 #include "bach.h"
 
+void run_bach(char* line);
+
 int main(int argc, char *argv[]) {
     char *line = "echo hello world";
-    run_bash(line);
+    run_bach(line);
     return 0;
 }
